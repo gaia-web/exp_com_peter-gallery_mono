421e2c6dc5599e82ac6f226071bcc1d9f865f232
Sean Wong <seanwong24@live.com>
2023-10-15 14:31:45 -0700

    add tab and tabs

---
+++ fe-utils/src/components/tab.ts
@@ -0,0 +1,84 @@
+import { LitElement, css, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { MyTabs } from "./tabs";
+
+const HTML_TAG_NAME = "my-tab";
+
+@customElement(HTML_TAG_NAME)
+export class MyTab extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      padding: 10px;
+      user-select: none;
+      cursor: pointer;
+      border-radius: var(--border-radius);
+      color: var(--item-color);
+    }
+
+    :host([selected]) {
+      background: var(--item-highlight-background);
+    }
+  `;
+
+  readonly #PARENT_TABS_ELEMENT_TAG_NAME = "my-tabs";
+
+  #parentTabsElement?: MyTabs;
+
+  /**
+   * Value of the tab.
+   */
+  @property({ reflect: true }) value?: string;
+
+  /**
+   * @internal
+   */
+  @property({ type: Boolean, reflect: true }) selected?: boolean;
+
+  get actualValue() {
+    return this.value ?? this.innerText;
+  }
+
+  constructor() {
+    super();
+    this.addEventListener(
+      "click",
+      () =>
+        this.#parentTabsElement &&
+        (this.#parentTabsElement.value = this.actualValue)
+    );
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.#parentTabsElement = this.#findParentTabsElement();
+    this.#parentTabsElement?.updateChildTabs();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.#parentTabsElement?.updateChildTabs();
+  }
+
+  render() {
+    return html` <slot></slot> `;
+  }
+
+  #findParentTabsElement() {
+    let walker = this.parentElement;
+    while (walker) {
+      if (
+        walker?.tagName === this.#PARENT_TABS_ELEMENT_TAG_NAME.toUpperCase()
+      ) {
+        return walker as MyTabs;
+      }
+      walker = walker.parentElement;
+    }
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [HTML_TAG_NAME]: MyTab;
+  }
+}
