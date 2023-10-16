f0d15ab70ed53ec62ef7c38c80704df6936fac38
Sean Wong <seanwong24@live.com>
2023-10-16 08:52:44 -0700

    update class names

--- fe/src/pages/fallback.ts
+++ fe/src/pages/fallback.ts
@@ -2,7 +2,7 @@ import { LitElement, css, html } from "lit";
 import { customElement } from "lit/decorators.js";
 
 @customElement("page-fallback")
-export class AppFallBackPage extends LitElement {
+export class FallBackPage extends LitElement {
   render() {
     return html`
       <h1>Fallback Page</h1>
@@ -17,6 +17,6 @@ export class AppFallBackPage extends LitElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    "page-fallback": AppFallBackPage;
+    "page-fallback": FallBackPage;
   }
 }
