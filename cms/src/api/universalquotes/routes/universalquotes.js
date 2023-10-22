4c128982850167231a8306c796213c7e3d6c9fbd
lidachao111222 <chao.li@usask.ca>
2023-10-23 00:10:26 +0800

    create database schema

---
+++ cms/src/api/universalquotes/routes/universalquotes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * universalquotes router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::universalquotes.universalquotes');
