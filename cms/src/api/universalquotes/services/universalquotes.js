f4c54475fee59f1baf448a4429704fb4f22232d3
ChaoChaoChao <chao.li@usask.ca>
2023-10-23 09:45:27 +0800

    feat(cms): cms init (#14)
    * modify dump
    
    * remove database if its already has.
    
    * create database schema
    
    * allow upload sql file

---
+++ cms/src/api/universalquotes/services/universalquotes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * universalquotes service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::universalquotes.universalquotes');
