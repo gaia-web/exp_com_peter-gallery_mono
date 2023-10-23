4b56a53bc46c364d93ae73e58ee512f5e53e7640
Sean Wong <seanwong24@live.com>
2023-10-22 19:51:46 -0600

    feat(fe-utils): add pagination component (#13)

---
+++ fe-utils/src/stories/pagination.stories.ts
@@ -0,0 +1,68 @@
+import { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+import "../components/pagination";
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type MyArgs = {
+  pageIndex: number;
+  pageCount: number;
+  maxNumberButtonCount: number;
+  pageNumberToLabel?: (pageNumber: number) => string;
+  onPageChange: (event: CustomEvent<number>) => void;
+};
+
+export default {
+  title: "Components/Pagination",
+  component: "util-pagination",
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    onPageChange: { action: "pageChange" },
+  },
+  render: (args) =>
+    html`
+      <util-pagination
+        page-index=${args.pageIndex}
+        page-count=${args.pageCount}
+        max-number-button-count=${args.maxNumberButtonCount}
+        @pageChange=${args.onPageChange}
+      ></util-pagination>
+    `,
+} satisfies Meta<MyArgs>;
+
+export const Default: StoryObj<MyArgs> = {
+  args: {
+    pageIndex: 1,
+    pageCount: 10,
+    maxNumberButtonCount: 5,
+  },
+};
+
+export const CustomTexts: StoryObj<MyArgs> = {
+  args: {
+    pageIndex: 1,
+    pageCount: 10,
+    maxNumberButtonCount: 5,
+    pageNumberToLabel: (x) => new Array(x + 1).join("/"),
+  },
+  render: (args) =>
+    html`
+      <util-pagination
+        page-index=${args.pageIndex}
+        page-count=${args.pageCount}
+        max-number-button-count=${args.maxNumberButtonCount}
+        @pageChange=${args.onPageChange}
+        .pageNumberToLabel=${args.pageNumberToLabel}
+      >
+        <div slot="first">First</div>
+        <div slot="previous">Previous</div>
+        <div slot="next">Next</div>
+        <div slot="last">Last</div>
+        <div slot="5">Five</div>
+        <div slot="any">Any</div>
+      </util-pagination>
+    `,
+};
