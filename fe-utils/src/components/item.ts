5e9fbfb77d99e036f90bdffdff6a4dac791aea26
Sean Wong <seanwong24@live.com>
2023-10-31 17:19:23 +0000

    update item docs

--- fe-utils/src/components/item.ts
+++ fe-utils/src/components/item.ts
@@ -1,6 +1,9 @@
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
+/**
+ * A common item.
+ */
 @customElement("util-item")
 export class UtilItemElement extends LitElement {
   static styles = css`
