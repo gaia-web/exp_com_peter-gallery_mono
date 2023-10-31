c6c6b81327bf0f42b6ee6fe0a33caa7be92fd228
Sean Wong <seanwong24@live.com>
2023-10-31 07:03:51 +0000

    add breadcrumb component

---
+++ fe-utils/src/components/breadcrumb.ts
@@ -0,0 +1,69 @@
+import { LitElement, css, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+/**
+ * Breadcrumbs helps to navgate within a hierarchy.
+ *
+ * @csspart item - the breadcrumb item.
+ * @csspart delimiter - the delimiter.
+ *
+ * @fires itemSelect - Occurs when an breadcrumb item is selected. The event `detail` prop gives the `path` of the selected item.
+ */
+@customElement("util-breadcrumb")
+export class UtilBreadcrumbElement extends LitElement {
+  static styles = css`
+    :host {
+      display: flex;
+    }
+
+    [part~="item"] {
+      margin: 0 10px;
+      cursor: pointer;
+    }
+
+    [part~="delimiter"] {
+      pointer-events: none;
+      user-select: none;
+    }
+  `;
+
+  /**
+   * The path as an array.
+   */
+  @property({ type: Array, reflect: true })
+  path: (string | number)[] = [];
+
+  /**
+   * The delimiter as an array.
+   */
+  @property({ reflect: true })
+  delimiter: string = ">";
+
+  render() {
+    return html`
+      ${this.path?.map(
+        (item, i) =>
+          html`<div
+              part="item"
+              @click=${() =>
+                this.dispatchEvent(
+                  new CustomEvent("itemSelect", {
+                    detail: this.path?.slice(0, i + 1),
+                  })
+                )}
+            >
+              ${item}
+            </div>
+            ${i < this.path.length - 1
+              ? html`<div part="delimiter">${this.delimiter}</div>`
+              : ""}`
+      )}
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "util-breadcrumb": UtilBreadcrumbElement;
+  }
+}
---
+++ fe-utils/src/stories/breadcrumb.stories.ts
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+import "../components/breadcrumb";
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type MyArgs = {
+  onItemSelect: (event: CustomEvent<(string | number)[]>) => void;
+};
+
+export default {
+  title: "Components/Breadcrumb",
+  component: "util-breadcrumb",
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    onItemSelect: { action: "itemSelect" },
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  render: (args) => html`
+    <util-breadcrumb
+      path='["One", "Two", "Three"]'
+      @itemSelect=${args.onItemSelect}
+    >
+    </util-breadcrumb>
+  `,
+} satisfies Meta<MyArgs>;
+
+export const Default: StoryObj<MyArgs> = {
+  args: {},
+};
