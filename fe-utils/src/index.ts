26efeaaa10db15ea10d14ab51447772c51471748
Sean Wong <seanwong24@live.com>
2023-10-31 17:17:52 +0000

    add progress button component

--- fe-utils/src/index.ts
+++ fe-utils/src/index.ts
@@ -2,5 +2,6 @@ export * from "./components/breadcrumb";
 export * from "./components/item";
 export * from "./components/map";
 export * from "./components/pagination";
+export * from "./components/progress-button";
 export * from "./components/tab";
 export * from "./components/tabs";
