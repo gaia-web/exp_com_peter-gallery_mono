7081f52549c367759bb4fc5faa18a85c5e649f37
willwei954 <41525364+willwei954@users.noreply.github.com>
2023-11-08 04:13:44 +0000

    Tried to fix animation

--- fe/src/pages/home.page.tsx
+++ fe/src/pages/home.page.tsx
@@ -4,7 +4,7 @@ export function HomePage() {
 
   return (
     <div>
-      <div class="threePics bottom-10 right-10 mb-2rem br-2rem ">
+      <div class="threePics bottom-10 right-10">
         <div class="parent text-5xl">
           <img src="/public/world.png"></img>
           <div >WORLD 世界</div>
@@ -20,16 +20,15 @@ export function HomePage() {
       </div>
 
       <div class="scaleDown h-85vh text-center slogan">
-        <div class="m-auto h-6rem relative flex justify-center items-end text-7xl">
+        <div class="fade-in-5s m-auto h-6rem relative flex justify-center items-end text-7xl">
           那颗我们所钟爱的
         </div>
-        <div class=" m-auto pt-0.5rem h-6rem text-7xl">
+        <div class="fade-in-6s m-auto pt-0.5rem h-6rem text-7xl">
           蔚蓝星球
         </div>
-        <div class="line bg-#0042ED m-auto h-0.2rem rounded-full ">
-        </div>
-        <div class="pt-2rem text-2xl">
-          以及，在她之上生生不息的我们
+         <div class="line bg-#0042ED m-auto h-0.2rem rounded-full"/>
+        <div class="slideDown pt-2rem text-2xl">
+            以及，在她之上生生不息的我们
         </div>
       </div>
     </div>
