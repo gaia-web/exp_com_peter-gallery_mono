77335af6803d499eb845ee6f1268f80d45d7c30e
willwei954 <41525364+willwei954@users.noreply.github.com>
2023-11-08 22:04:30 -0500

    feat(fe): create home page and animations (#22)
    * Basic components
    
    * add three pics and animations
    
    * Tried to fix animation
    
    * update animations
    
    * modify animation duration
    
    * Replace picture with link
    
    ---------
    
    Co-authored-by: Sean Wong <seanwong24@live.com>

--- fe/src/pages/home.page.tsx
+++ fe/src/pages/home.page.tsx
@@ -1,12 +1,50 @@
-export function HomePage() {
+import "./home.css";
 
+export function HomePage() {
   return (
-    <>
-      <div>
-        <p>
-          Home page
-        </p>
+    <div>
+      <div class="threePics bottom-10 right-10 later" style="--animation-delay: 6s;">
+        <div class="parent text-5xl">
+          <img src="//picsum.photos/1200/800"></img>
+          <div>WORLD 世界</div>
+        </div>
+        <div class="parent text-5xl">
+          <img src="//picsum.photos/1200/800"></img>
+          <div>PEOPLE 众生</div>
+        </div>
+        <div class="parent text-5xl">
+          <img src="//picsum.photos/1200/800"></img>
+          <div>SELVES 我们</div>
+        </div>
       </div>
-    </>
+
+      <div
+        class="fixed bottom-0 left-0 text-center translate-x-[var(--initial-translate-x)] translate-y-[var(--initial-translate-y)] scale-[var(--initial-scale)] slogan"
+        style="--animation-delay: 6s; --initial-translate-x: calc(50vw - 50%); --initial-translate-y: calc(50% - 50vh); --initial-scale: 1.5; --left-offset: 50px; --bottom-offset: 50px;"
+      >
+        <div
+          class="opacity-0 my-2 slogan-text animate-delay-1000 font-size-[2em]"
+          style="--inital-animation-delay: 1s; --index: 0;"
+        >
+          那颗我们所钟爱的
+        </div>
+        <div
+          class="opacity-0 my-2 slogan-text animate-delay-2000 font-size-[2em]"
+          style="--inital-animation-delay: 1s; --index: 1;"
+        >
+          蔚蓝星球
+        </div>
+        <div
+          class="opacity-0 my-2 bg-#0042ED h-[2px] slogan-text animate-delay-3000"
+          style="--inital-animation-delay: 1s; --index: 2;"
+        />
+        <div
+          class="opacity-0 my-2 slogan-text animate-delay-4000 font-size-[1em]"
+          style="--inital-animation-delay: 1s; --index: 3;"
+        >
+          以及，在她之上生生不息的我们
+        </div>
+      </div>
+    </div>
   );
 }
