dc221d3bf7e07df44f032116c192e45a91951f91
willwei954 <41525364+willwei954@users.noreply.github.com>
2024-01-01 01:02:41 +0000

    about us skeleton

--- fe/src/pages/selves.page.tsx
+++ fe/src/pages/selves.page.tsx
@@ -5,8 +5,18 @@ export function SelvesPage({ routerInfo }: PageProps) {
   return (
     <>
       <SelvesHeaderView routerInfo={routerInfo} />
-      <div>
-        <p>Selves page</p>
+      <div class="grid grid-cols-8 gap-1 pl-10vw pr-10vw">
+        <p class="col-span-5">Selves page</p>
+        <div class="col-span-3">
+          <img class="w-full rounded-3xl" src="//picsum.photos/seed/any/800/800" />
+          <div class="flex justify-between m-1rem">
+            <img src="/icons/Facebook.svg"/>
+            <img src="/icons/Twitter.svg"/>
+            <img src="/icons/Linkedin.svg"/>
+            <img src="/icons/Wechat.svg"/>
+            <img src="/icons/Weboo.svg"/>
+          </div>
+        </div>
       </div>
     </>
   );
