4daeb17d2e586a28cbcbdf49e8de58e267ee7dc3
lidachao111222 <chao.li@usask.ca>
2024-01-21 09:56:18 +0800

    create new cms

---
+++ cms/src/api/article-list-page-slogan/controllers/article-list-page-slogan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-list-page-slogan controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::article-list-page-slogan.article-list-page-slogan');
---
+++ cms/src/api/article-list-page-slogan/routes/article-list-page-slogan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-list-page-slogan router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::article-list-page-slogan.article-list-page-slogan');
